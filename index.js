dd4be4ba378bd562c0d5d35cf5d37c7d22655b3a
Kyle Treman <jkt290@nyu.edu>
2017-11-15 16:04:44 -0500

    working

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-(function() {
-  'use strict';
 
-  // YOUR CODE HERE
-})();
+let stop = document.getElementById('stopButton');
+let slow = document.querySelector('#slowButton');
+let go = document.querySelector('#goButton');
+
+stop.addEventListener('click', function() {
+  let stopB = document.getElementById('stopLight');
+  stopB.classList.toggle('stop');
+})
+slow.addEventListener('click', function() {
+  let slowB = document.getElementById('slowLight');
+  slowB.classList.toggle('slow');
+
+})
+go.addEventListener('click', function() {
+  let goB = document.getElementById('goLight')
+  goB.classList.toggle('go');
+})
+let buttons = document.getElementsByClassName('button');
+
+for (let i = 0; i < buttons.length; i++) {
+  // console.log('hi');
+
+  let button = buttons[i];
+  button.addEventListener('mouseenter', function() {
+    console.log(button.textContent + ' bulb on');
+  })
+  button.addEventListener('mouseleave', function() {
+    console.log(button.textContent + ' bulb off');
+  })
+};
